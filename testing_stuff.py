76f7ca9862bcbfc96c6934cd7c0d8cc8079d2601
hamemuh <muhammed.ah@hotmail.com>
2021-01-06 13:47:17 +0000

    sentiment analysis section completed

--- testing_stuff.py
+++ testing_stuff.py
@@ -1,9 +1,5 @@
 import numpy as np
-mat1 = np.random.rand(2,1)
-mat2 = np.random.rand(2,3)
-# print(np.dot(mat2, mat1))
-print("*" * 10)
-# print(np.matmul(mat2, mat1))
-print("*" * 10)
-print(mat1.T.shape)
-print(np.matmul(mat2.T, mat1))
+l = ['excellent film', 'really poorly executed']
+words = {word for review in l for word in review.split(' ')}
+for word in words:
+    print(word)
